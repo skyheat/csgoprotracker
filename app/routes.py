afbfb873bef362f3c854cceb0dc48ec5e6d5801b
skyheat <skyheatcp@gmail.com>
2021-03-17 14:20:36 +0800

    Cleaning code up

--- app/routes.py
+++ app/routes.py
@@ -29,7 +29,6 @@ def index():
     teamNames = []
     teamNamesNA= []
     for i in euhub_matches['items']:
-        #print(i['match_id'])
         if(i['status'] != "CANCELLED"): 
             teamname1 = i["teams"]["faction1"]["name"]
             teamname2 = i["teams"]["faction2"]["name"]
@@ -39,7 +38,6 @@ def index():
         else:
             continue
     for i in nahub_matches['items']:
-        #print(i['match_id'])
         if(i['status'] != "CANCELLED"): 
             teamname1 = i["teams"]["faction1"]["name"]
             teamname2 = i["teams"]["faction2"]["name"]
@@ -54,17 +52,12 @@ def index():
 @app.route("/search/", methods=["POST"])
 def searchPlayer():
     playername = request.form['searchname']
-    #print(playername)
     playerList = faceit_data.search_players(playername, 'csgo', None, 0, 5)
-    #print(playerList)
-    #print(faceit_data.search_players("GeT_RiGhT", 'csgo'))
     playerarray = []
     for i in playerList['items']:
-        #print(i)
         playerarray.append(i)
         if(i['verified']):
             return redirect(url_for('player', playerid=i['player_id']))
-    #print(playerarray)
 
     return render_template('search.html', title='Player Search', playername=playername, playerarray=playerarray)
 
@@ -97,34 +90,19 @@ def match(matchid):
     count2 = 0
     for c in range(team1len):
         playerstats = match_stats['rounds'][0]['teams'][0]['players'][c]['player_stats']
-       # print(playerstats)
         playerstats['Player'] = team1[count]
-        #print(playerstats)
         reorderedstats = {k: playerstats[k] for k in reordersample}
         team1stats.append(reorderedstats)
         count = count+1
     for c in range(team2len):
         playerstats = match_stats['rounds'][0]['teams'][1]['players'][c]['player_stats']
-        #print(playerstats)
         playerstats['Player'] = team2[count2]
-        #print(playerstats)
         reorderedstats = {k: playerstats[k] for k in reordersample}
-        #print(playerstats)
         team2stats.append(reorderedstats)
         count2 = count2 + 1
     team1stats = sorted(team1stats, key = lambda i: int(i['Kills']), reverse=True)
     team2stats = sorted(team2stats, key = lambda i: int(i['Kills']), reverse=True)
-    #print(sorted(team2stats, key = lambda i: i['Kills'], reverse=True))
-
-    #print(match_score)   
-    #print(team1, team1stats)
-    #print(team2, team2stats)
     demourl = match_details["demo_url"]
-    #print(demourl[0])
-    #reordered_dict = {k: sample_dict[k] for k in desired_order_list}
-    #team1 = {'team1' : team1}
-    #team2 = {'team2' : team2}
-    #playername_stats1 = zip(team1, team1stats, team1ids), playername_stats2 = zip(team2, team2stats, team2ids)
     matchmap = match_details['voting']['map']['pick']
     matchmap = {"matchmap" : matchmap}
     
@@ -138,16 +116,11 @@ def help():
     
 @app.route("/player/<playerid>")
 def player(playerid):
-    #print(playerid)
     playername = faceit_data.player_id_details(playerid)['nickname']
-    #print(playername)
     playermatches = faceit_data.player_matches(playerid, "csgo", None, None, 0, 20)
-    #matchstats = faceit_data.match_stats()
-    #playerstats = []
     matches = []
     matchmaps = []
     count = 0
-    #print(playermatches)
     for i in playermatches['items']:
         if(i['competition_id'] == "74caad23-077b-4ef3-8b1d-c6a2254dfa75" or i['competition_id'] == "748cf78c-be73-4eb9-b131-21552f2f8b75"):
             try: 
@@ -158,11 +131,5 @@ def player(playerid):
             except KeyError:
                 continue
     message = "User has no matches on FPL Pro Hubs"
-    #print(matchmaps)
-    #playerstats = {'playerstats' : playerstats}
-    #playername = playerdetails['nickname']
-    #playername = {'playername' : playername}
-    #playerid = {'playerid' : playerid}
-    #matches = {'matches' : matches}
     
     return render_template("playerpage.html", title='Player Page', playername=playername, match_map=zip(matches, matchmaps), message=message, count=count)
